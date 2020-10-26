e547299e56100c733f7c11215657297c91bc1600
Noor <noormir1295@gmail.com>
2020-10-26 13:19:48 -0400

    finishing models for workout and index export

--- models/workout-tracker.js
+++ models/workout-tracker.js
@@ -50,3 +50,13 @@ const workoutSchema = new Schema(
     }
   }
 );
+
+workoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
