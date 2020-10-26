6e55cfb3c1545cf287ae19408a23b768fd5ff6e7
Noor <noormir1295@gmail.com>
2020-10-26 14:21:01 -0400

    deploying

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
